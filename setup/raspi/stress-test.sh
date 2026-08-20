b77e6a6c44ecb56244fb1c983b1a1ec05a657f03
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-08-20 21:59:57 +0200

    fix: making mac address dynamic

--- setup/raspi/stress-test.sh
+++ setup/raspi/stress-test.sh
@@ -2,17 +2,17 @@
 
 set -euo pipefail
 
+MAC_ADDRESS="$1"
+
 if [ ! -d "./output" ]; then
 	mkdir ./output
 fi
 
 for i in {1..4}; do
 	stress-ng --cpu $i --timeout 35 &
-	./powerspy.py -i 1 -d 30 -f "./output/raspi-cpu-$i.csv"  00:06:66:4D:F5:F3
+	./powerspy.py -i 1 -d 30 -f "./output/raspi-cpu-$i.csv" $1 
 	sleep 5
 done
 
-./powerspy.py -i 1 -d 30 -f ./output/raspi-idle.csv  00:06:66:4D:F5:F3
-
-
+./powerspy.py -i 1 -d 30 -f ./output/raspi-idle.csv $1 
 
