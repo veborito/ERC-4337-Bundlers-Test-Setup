49bfd4f006716b90ba84b29f76b5e85db8b125bc
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2026-08-24 08:41:09 +0200

    fix: variable name

--- setup/raspi/stress-test.sh
+++ setup/raspi/stress-test.sh
@@ -9,10 +9,10 @@ if [ ! -d "./output" ]; then
 fi
 
 for i in {1..4}; do
-	stress-ng --cpu $i --timeout 35 &
-	./powerspy.py -i 1 -d 30 -f "./output/raspi-cpu-$i.csv" $1 
+	stress-ng --cpu $i --timeout 180 &
+	./powerspy.py -i 1 -d 180 -f "./output/raspi-cpu-$i.csv" "$MAC_ADDRESS"
 	sleep 5
 done
 
-./powerspy.py -i 1 -d 30 -f ./output/raspi-idle.csv $1 
+./powerspy.py -i 1 -d 180 -f ./output/raspi-idle.csv "$MAC_ADDRESS" 
 
