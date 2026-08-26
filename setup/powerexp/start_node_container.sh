c9ff22321c6f403f83a054516332a217b1733bbe
Boris Verdecia Echarte <borito@Sunny-2.local>
2026-08-26 18:08:37 +0200

    fix: file path

--- setup/powerexp/start_node_container.sh
+++ setup/powerexp/start_node_container.sh
@@ -1,25 +1,27 @@
+#!/usr/bin/env bash
+
 set -euo pipefail
 
-OUT_FILE="${1:-containers_id.txt}"
+OUT_FILE="${1:-container_ids.txt}"
 
 # Optional cleanup if old containers with the same names exist
 sudo docker rm -f anvil >/dev/null 2>&1 || true
 
 ANVIL_ID=$(
-  sudo docker run --rm -d \
-    --name anvil \
-    -v ~/powerexp/state/state.json:/var/lib/anvil/state.json \
-    -v ~/powerexp/dump/state.json:/var/lib/anvil/dump/state.json \
-    -p 8545:8545 \
-    anvil-debian-slim:local 
+	sudo docker run --rm -d \
+		--name anvil \
+		-v ~/powerexp/state/state.json:/var/lib/anvil/state.json \
+		-v ~/powerexp/dump/state.json:/var/lib/anvil/dump/state.json \
+		-p 8545:8545 \
+		anvil-debian-slim:local
 )
 
 {
-  echo "timestamp=$(date --iso-8601=seconds)"
-  echo "anvil=$ANVIL_ID"
-  echo
-  echo "anvil_cgroup=$(cat /proc/$(sudo docker inspect -f '{{.State.Pid}}' anvil)/cgroup | cut -d: -f3)"
-} > "$OUT_FILE"
+	echo "timestamp=$(date --iso-8601=seconds)"
+	echo "anvil=$ANVIL_ID"
+	echo
+	echo "anvil_cgroup=$(cat /proc/$(sudo docker inspect -f '{{.State.Pid}}' anvil)/cgroup | cut -d: -f3)"
+} >"$OUT_FILE"
 
 echo "Started container:"
 echo "  anvil: $ANVIL_ID"
