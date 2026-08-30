fa05ae5cb8b24519c01687f4a20e17b24822290a
Boris Verdecia Echarte <borito@Sunny-2.local>
2026-08-30 18:22:22 +0200

    fix: more time for bundler to fetch startup info

--- client/raspi_tests/runRaspiTest.sh
+++ client/raspi_tests/runRaspiTest.sh
@@ -37,7 +37,7 @@ ssh "$BUNDLER_MACHINE" "cd ~/ERC-4337-Bundlers-Test-Setup/scripts/raspi && ./sta
 
 echo "everything started smoothly..."
 
-sleep 40
+sleep 60
 
 cd ~/ERC-4337-Bundlers-Test-Setup/client/dist && node transferUserOpRoundsThrottled.js "$ROUNDS_TOTAL" "$SCA_NUMBER" "$THROTTLE_TIME" "$BLOCK_TIME"
 
